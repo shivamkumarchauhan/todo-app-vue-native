6c83a5f5350720e4b56f2e9ef1486e4ec7c2a371
shivamkumarchauhan1 <shivamkumarchauhan.com@gmail.com>
2021-01-30 15:58:18 +0530

    completing task

---
+++ App.vue
@@ -0,0 +1,134 @@
+<template>
+	<view class="container">
+		<Statusbar color="orange"/>
+		<Header title="Todo App" />
+
+		<view class="input-container">
+			<text-input class="input" v-model="newTodoText" placeholder="Enter Your Task" />
+			<touchable-opacity class="add-btn" :on-press="newTodo">
+				<text class="btn-text">Add</text>
+			</touchable-opacity>
+		</view>
+
+		<view class="todo" v-for="todo in todos" :key="todo.id">
+			<touchable-opacity :on-press="() => toogleDone(todo.id)">
+				<text class="todo-text done" v-if="todo.done">{{ todo.title }}</text>
+				<text class="todo-text" v-else>{{ todo.title }}</text>
+			</touchable-opacity>
+			<touchable-opacity :on-press="() => removeTodo(todo.id)">
+				<text class="remove-btn-text">Remove</text>
+			</touchable-opacity>
+		</view>
+	</view>
+</template>
+
+<script>
+	import Statusbar from './components/Statusbar';
+	import Header from './components/Header';
+	
+    export default {
+		data(){
+			return {
+				newTodoText: '',
+				todos:[
+					{
+						id:0,
+						title:'Go Shopping',
+						done:false
+					},
+					{
+						id:1,
+						title:'Go to school',
+						done: false
+					},
+					{
+						id:2,
+						title:'Go Joker',
+						done: false
+					}
+				]
+			}
+		},
+       components: {
+		   Statusbar,
+		   Header
+	   },
+	   methods:{
+		   newTodo(){
+			   if(this.newTodoText != ''){
+					this.todos.push({
+						id: this.todos.length + 1,
+						title: this.newTodoText,
+						done: false
+					});
+			   }
+
+			   this.newTodoText = '';
+		   }, 
+		   toogleDone(id){
+			   this.todos = this.todos.map(todo => {
+				   if(todo.id == id) todo.done = !todo.done;
+				   return todo;
+			   })
+		   },
+		   removeTodo(id){
+			   this.todos = this.todos.filter(todo => todo.id != id)
+		   }
+	   }
+	}
+</script>
+
+<style>
+	.container {
+		background-color: white;
+		flex: 1;
+	}
+
+	.input-container{
+		flex-direction: row;
+		width: 100%;
+		justify-content: center;
+		align-items: stretch;
+	}
+
+	.input{
+		flex: 1;
+		height: 40px;
+		background-color: rgb(221, 231, 200);
+		font-size: 25px;
+		color: #000000;
+	}
+
+	.add-btn{
+		width:100px;
+		height: 40px;
+		background-color: brown;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.btn-text {
+		font-size: 20px;
+		font-weight: 900;
+	}
+
+	.todo {
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 15px;
+	}
+
+	.todo-text{
+		font-size: 18px;
+		color: rgb(13, 56, 117);
+	}
+
+	.done{
+		color: #2cbf90;
+	}
+
+	.remove-btn-text{
+		font-size: 18px;
+		color: red;
+	}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <view class="header">
+        <text class="title"> {{ title }}</text>
+    </view>
+</template>
+
+<script>
+export default {
+    props:['title']
+}
+</script>
+
+<style>
+    .header{
+        height: 60px;
+        background-color:rgb(0, 0, 0);
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title{
+        color: green;
+        font-size: 30px;
+        font-weight: 900;
+        text-transform: uppercase;
+    }
+</style>
